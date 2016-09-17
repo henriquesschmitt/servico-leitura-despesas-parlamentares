40c440d0eaa662dc8bb2814610fc6be9fae06645
Henrique Schmitt <henriquesschmitt@gmail.com>
2016-09-16 23:19:53 -0300

    Gravação de informações de parlamentares.

--- src/com/service/readxml/ReadXmlService.java
+++ src/com/service/readxml/ReadXmlService.java
@@ -12,22 +12,26 @@ public class ReadXmlService {
 
     public static void main(String[] args) {
                 
-        /** BAIXA E SALVA O ARQUIVO DE DESPESAS DO SENADO FEDERAL **/
-        DownloadFile.downloadAndSaveFile("http://www.senado.gov.br/transparencia/LAI/verba/2016.csv", "senado.csv");
-        
-        /** LE E GRAVA DESPESAS DO ARQUIVO NA BASE DE DADOS **/
-        List<Despesa> expensesList = ReadAndPersistFile.readFileExpensesSenado("./arquivos/senado.csv", "s");
-        
+//        /** BAIXA E SALVA O ARQUIVO DE DESPESAS DO SENADO FEDERAL **/
+//        DownloadFile.downloadAndSaveFile("http://www.senado.gov.br/transparencia/LAI/verba/2016.csv", "senado.csv");
+//        
+//        /** LE E GRAVA DESPESAS DO ARQUIVO NA BASE DE DADOS **/
+//        List<Despesa> expensesList = ReadAndPersistFile.readFileExpensesSenado("./arquivos/senado.csv", "s");
+//        
         EntityManagerProducer entityManagerProducer = new EntityManagerProducer();
-        DespesaDAO despesaDAO = new DespesaDAO();
-        
-        /** LIMPA A TABELA ANTES DE PERSISTIR OS DADOS **/
-        despesaDAO.excluir(entityManagerProducer.create());        
-                
-        /** GRAVA TODAS AS DESPESAS LIDAS DO PORTAL DA TRANSPARENCIA NA BASE DE DADOS **/
-        System.out.println("GRAVA TODAS AS DESPESAS LIDAS DO PORTAL DA TRANSPARENCIA NA BASE DE DADOS");
-        for(Despesa despesa_corrente : expensesList){
-        	despesaDAO.salvar(entityManagerProducer.create(), despesa_corrente);
-        }        
+//        DespesaDAO despesaDAO = new DespesaDAO();
+//        
+//        /** LIMPA A TABELA ANTES DE PERSISTIR OS DADOS **/
+//        despesaDAO.excluir(entityManagerProducer.create());        
+//                
+//        /** GRAVA TODAS AS DESPESAS LIDAS DO PORTAL DA TRANSPARENCIA NA BASE DE DADOS **/
+//        System.out.println("GRAVA TODAS AS DESPESAS LIDAS DO PORTAL DA TRANSPARENCIA NA BASE DE DADOS");
+//        for(Despesa despesa_corrente : expensesList){
+//        	despesaDAO.salvar(entityManagerProducer.create(), despesa_corrente);
+//        }       
+    	
+    	GetDadosParlamentares dadosParlamentares = new GetDadosParlamentares();
+    	dadosParlamentares.sendGetParlamentaresDados(entityManagerProducer.create());
+    	
     }    
 }
